d04f0f56400020f05e524b20da5b12e845456a3f
Ming <dingmo@uw.edu>
2024-07-13 14:55:26 +0100

    NLTK fix attempt #1

--- bs_and_nlp.py
+++ bs_and_nlp.py
@@ -1,3 +1,13 @@
+import os
+import nltk
+
+nltk_data_dir = "./resources/nltk_data_dir/"
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.clear()
+nltk.data.path.append(nltk_data_dir)
+nltk.download("vader_lexicon", download_dir=nltk_data_dir)
+
 import requests
 import nltk
 from bs4 import BeautifulSoup
